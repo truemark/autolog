63583bbba57a6b48dbb60e48d39eb84b8cbd1fcd
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-12-27 11:23:48 -0700

    Adding versioning

--- cdk/lib/auto-log-stack.ts
+++ cdk/lib/auto-log-stack.ts
@@ -3,6 +3,7 @@ import {Construct} from 'constructs';
 import {AutoLogConstruct} from './auto-log-construct';
 import {CfnOutput} from 'aws-cdk-lib';
 import * as globals from './globals';
+import * as p from '../../package.json';
 
 export class AutoLogStack extends ExtendedStack {
   readonly name: CfnOutput;
@@ -10,6 +11,8 @@ export class AutoLogStack extends ExtendedStack {
   readonly url: CfnOutput;
   constructor(scope: Construct, id: string, props: ExtendedStackProps) {
     super(scope, id, props);
+    this.outputParameter('Name', 'AutoDump');
+    this.outputParameter('Version', p.version);
     new AutoLogConstruct(this, 'AutoLog');
     this.name = this.outputParameter('Name', globals.NAME);
     this.version = this.outputParameter('Version', globals.NAME);
